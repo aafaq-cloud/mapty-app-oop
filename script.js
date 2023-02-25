d424e315329a5d748ec2742104189f852af55b83
aafaq-cloud <Aafaqsikandar@gmail.com>
2023-02-25 22:37:38 +0500

    implement inheritance

--- script.js
+++ script.js
@@ -14,7 +14,65 @@ const inputElevation = document.querySelector('.form__input--elevation');
 // Global variables
 // let map, mapEvent;
 
+// Parent Class of Running and Cycling
+class Workout {
+  // We need an id (unique identifier)
+  date = new Date();
+  // Create id using library
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    // this.date = ...
+    // this.id = ...
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+
+    this.calcPace();
+  }
+
+  // Method to calculate pace
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    // The speed is opposite to the pace
+    this.speed = this.distance / (this.duration / 60);
+
+    return this.speed;
+  }
+}
+
+const running = new Running([39, -12], 5.2, 24, 178);
+const cycling = new Cycling([39, -12], 27, 95, 523);
+
+console.log(running);
+console.log(cycling);
+
 // Blueprint
+//////////////////////////////////////////
+// APPLICATION ARCHITECTURE
 class App {
   // Private class fields
   // Private instance properties so the properties that are available to all of the objects created through this class
@@ -133,130 +191,3 @@ class App {
 
 // Create an actual object from class
 const app = new App();
-
-// if (navigator.geolocation) {
-//   navigator.geolocation.getCurrentPosition(
-//     function (position) {
-//       console.log('Position:');
-//       console.log(position);
-//       console.log(position.coords);
-
-//       // Object destructuring
-//       const { latitude, longitude } = position.coords;
-//       console.log(latitude);
-//       console.log(longitude);
-
-//       //   https://www.google.com/maps/@31.4497036,74.4173874
-//       console.log(`https://www.google.com/maps/@${latitude},${longitude})`);
-
-//       const coords = [latitude, longitude];
-
-//       //   Leaflet Library
-//       //   var map = L.map('map').setView([51.505, -0.09], 13);
-//       map = L.map('map').setView(coords, 13);
-
-//       //   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//       //     attribution:
-//       //       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-//       //   }).addTo(map);
-//       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-//         attribution:
-//           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-//       }).addTo(map);
-
-//       //   L.marker([51.5, -0.09])
-//       //     .addTo(map)
-//       //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-//       //     .openPopup();
-//       //   L.marker(coords)
-//       //     .addTo(map)
-//       //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-//       //     .openPopup();
-
-//       console.log('Map:');
-//       console.log(map);
-
-//       //   Add event listener on map
-//       //   map.on('click', function (e) {
-//       //     console.log(e);
-//       //     new L.Marker([e.latlng.lat, e.latlng.lng]).addTo(map);
-//       //   });
-
-//       // User clicks on map
-//       map.on('click', function (mapE) {
-//         mapEvent = mapE;
-//         console.log(mapEvent);
-//         console.log(mapEvent.latlng);
-
-//         const { lat, lng } = mapEvent.latlng;
-//         console.log(lat, lng);
-
-//         // Render form
-//         form.classList.remove('hidden');
-
-//         //   Focus on distance input field
-//         inputDistance.focus();
-
-//         // L.marker([lat, lng]).addTo(map).bindPopup('Workout').openPopup();
-//         // L.marker([lat, lng])
-//         //   .addTo(map)
-//         //   .bindPopup(
-//         //     L.popup({
-//         //       maxWidth: 250,
-//         //       minWidth: 100,
-//         //       autoClose: false,
-//         //       closeOnClick: false,
-//         //       className: 'running-popup',
-//         //     })
-//         //   )
-//         //   .setPopupContent('Workout')
-//         //   .openPopup();
-
-//         // Render form
-//         //   DOM Manipulation
-//       });
-//     },
-//     function () {
-//       alert('Could not get your position!');
-//     }
-//   );
-// }
-
-// form.addEventListener('submit', function (e) {
-//   e.preventDefault();
-//   // Display marker
-//   console.log('Form is submitted!');
-
-//   // Clear input fields
-//   inputDistance.value =
-//     inputDuration.value =
-//     inputCadence.value =
-//     inputElevation.value =
-//       '';
-
-//   // Diaplay marker
-//   // Render workout on map
-//   const { lat, lng } = mapEvent.latlng;
-//   console.log(lat, lng);
-
-//   L.marker([lat, lng]).addTo(map).bindPopup('Workout').openPopup();
-//   L.marker([lat, lng])
-//     .addTo(map)
-//     .bindPopup(
-//       L.popup({
-//         maxWidth: 250,
-//         minWidth: 100,
-//         autoClose: false,
-//         closeOnClick: false,
-//         className: 'running-popup',
-//       })
-//     )
-//     .setPopupContent('Workout')
-//     .openPopup();
-// });
-
-// inputType.addEventListener('change', function (e) {
-//   console.log(e.target.value);
-//   inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-//   inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-// });
